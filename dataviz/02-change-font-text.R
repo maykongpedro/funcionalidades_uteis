a3de95890cc774e488c0fca59a1201b730173fe1
maykongpedro <maykonglaffite@gmail.com>
2021-09-10 23:57:15 -0300

    add opção 1

--- dataviz/02-change-font-text.R
+++ dataviz/02-change-font-text.R
@@ -3,6 +3,9 @@
 
 # Opção 1 -----------------------------------------------------------------
 
+# Adicionar fonte de texto com sysfonts e usar ela com showtext
+sysfonts::font_add_google("Crimson Text","Crimson")
+showtext::showtext_auto()
 
 
 
