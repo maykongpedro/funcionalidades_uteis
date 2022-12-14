5c29574cb6d1913728f05223dc1b3f05c28fb62b
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-12-13 23:05:10 -0300

    add data and formulas in the workbook

--- 01-export-data-in-excel.R
+++ 01-export-data-in-excel.R
@@ -30,3 +30,64 @@ filename <- glue::glue("{here::here()}/others/openxlsx_workbook.xlsx")
 
 # salvar workbook
 openxlsx::saveWorkbook(wb, file = filename, overwrite = TRUE)
+
+
+
+# Write data in the workbook ----------------------------------------------
+
+# escrever dados na planilha - diamonds
+openxlsx::writeData(
+    wb = wb,
+    sheet = "diamond_data",
+    x = diamond_xl,
+    startCol = 2,
+    startRow = 1
+)
+
+
+# escrever dados na planilha - starwars
+openxlsx::writeData(
+    wb = wb,
+    sheet = "starwars_data",
+    x = dplyr::starwars,
+    rowNames = TRUE
+)
+
+# adicionar cabeçalhos
+openxlsx::writeData(
+    wb = wb,
+    sheet = "diamond_data",
+    x = "Preço Alvo",
+    startCol = 7
+)
+
+# adicionar cabeçalhos
+openxlsx::writeData(
+    wb = wb,
+    sheet = "diamond_data",
+    x = "Diferença de Preço",
+    startCol = 8
+)
+
+# adicionar uma fórmula para calcular a diferença
+dif_preco <- glue::glue("G{2:6}-E{2:6}")
+dif_preco
+
+openxlsx::writeFormula(
+    wb = wb,
+    sheet = "diamond_data",
+    x = dif_preco,
+    startCol = 8 ,
+    startRow = 2
+)
+
+# salvar workbook
+openxlsx::saveWorkbook(wb, file = filename, overwrite = TRUE)
+
+
+
+
+
+
+
+
